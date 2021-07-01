6099f65408c7c66790a1e19e7a64b554b68d6eb6
Steven Del Rosario <stevendelro@pm.me>
2021-07-01 08:56:53 -0500

    moved root GET handler below main route handlers

--- server/server.js
+++ server/server.js
@@ -2,15 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-// const cors = require('cors');
-
-// app.use(cors());
-
-// app.use('/Login', (req, res) => {
-//   res.send({
-//     token: 'edd'
-//   })
-// })
 
 app.use(express.json()); //testing
 
@@ -18,16 +9,13 @@ const apiRouter = require('./routes/apiRoutes');
 const loginRouter = require('./routes/loginRoutes');
 const signupRouter = require('./routes/signupRoutes');
 
-app.get('/', (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
-});
-
 app.use('/api', apiRouter);
-
 app.use('/login', loginRouter);
-
 app.use('/signup', signupRouter);
 
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../client/static/index.html'));
+});
 app.use((req, res) => res.sendStatus(404));
 
 app.use((err, req, res, next) => {
